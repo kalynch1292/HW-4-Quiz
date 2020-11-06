5686b7746db815f231fa85b12e1b3609ea97822d
Kayla Lynch <kalynch1292@gmail.com>
2020-11-05 20:02:00 -0500

    working on arrays1

--- script.js
+++ script.js
@@ -2,31 +2,79 @@
 
 /// Quiz Questions ///
 
+// var nextBtn = document.querySelector(#next);
+
+// nextbtn.addEventListener("click," startQuiz);
+
+// start quiz //
+
+
+
+
+
+function quizLoop() {
+  for(var i = 0 < quizQuestions[prompt].options.length; i++;) {
+    //display current question to user//
+
+
+  }
+}
+
+
+
+
+// function quizLoop () {
+//   // confused here but//
+//   //this loops through question array//
+//   for (var i = 0; i < questions.length; i++) {
+//     questionsDiv.innerHTML = "";
+
+//     // Appends question title only//
+
+//     var userQuestion = questions[questionIndex].prompt;
+//     var userChoices = choices[questionIndex].choices;
+//     questionsDiv.textContent = userQuestion;
+//   }
+// }
+
+
+
+
 var quizQuestions = [
   {
-    question: "What is a while loop?",
+    prompt: "What is a while loop?",
     options: ["A While loop never stops", "A while loop executes a set of statements as long as a condition is true, Does not require a function", "Pass"],
-    correctAnswer: "A while loop executes a set of statements as long as a condition is true, Does not require a function"
+    answerIndex: 1
   },
 
   {
-    question: "How is a while loop different from an if loop",
-    choiceA: ["An if loop only runs through one block of code a certain number of times but a while loop runs until the statement is false.", "They are the same.", "if loops do not exist.", "Pass."],
-    correctAnswer: "A"
+    prompt: "How is a while loop different from an if loop",
+    options: ["An if loop only runs through one block of code a certain number of times but a while loop runs until the statement is false.", "They are the same.", "if loops do not exist.", "Pass."],
+    answerIndex: 0
   },
 
   {
-    question: "What is the difference between " == " and " === "",
-    choiceA: ["No difference", " '==' does not check datatype of variable but '===' does", "i'==' is only used for functions.", "Pass."],
-    correctAnswer: "B"
+    prompt: "What is the difference between " == " and " === "",
+    options: ["No difference", " '==' does not check datatype of variable but '===' does", " '==' is only used for functions.", "Pass."],
+    answerIndex: 1
   },
 
   {
-    question: "What does the term Booleans mean?",
-    choiceA: ["A type of loop", "Another term for a Variable", "Datatype that only takes 'true' or 'false' values", "Pass."],
-    correctAnswer: "B"
-  },
+    prompt: "What does the term Booleans mean?",
+    options: ["A type of loop", "Another term for a Variable", "Datatype that only takes 'true' or 'false' values", "Pass."],
+    answerIndex: 3
+  }
+
 ];
+
+
+
+
+
+
+
+
+
 
 //more variables//
 var score = 0;
@@ -36,14 +84,15 @@ var timerEl = document.querySelector("#timeLeft");
 var questionsDiv = document.querySelector("#questionsDiv");
 var wrapper = document.querySelector("#wrapper");
 var secondsLeft = 60;
+var button = document.getElementById("startQuiz");
 
 setTime();
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft --;
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
     timerEl.textContent = "Time Left " + secondsLeft;
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
     }
 
